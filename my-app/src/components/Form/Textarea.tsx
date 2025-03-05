c5d24f6a9cad7ec646cf5fe19e8f135a6b30b18c
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2025-03-05 18:12:32 +0900

    feat: textarea 컴포넌트 추가

--- my-app/src/components/Form/Textarea.tsx
+++ my-app/src/components/Form/Textarea.tsx
@@ -1,7 +1,31 @@
-import "../../styles/Button.css";
+import "../../styles/Form.css";
+import { TextareaProps } from "./types";
 
-const Textarea = () => {
-  return <div className="add-button">+ 추가</div>;
+const Textarea = ({
+  label,
+  name,
+  value,
+  onChange,
+  required,
+  placeholder,
+}: TextareaProps) => {
+  return (
+    <div className="form-field">
+      <label htmlFor={name} className="field-label">
+        {label}
+        {required && <span className="required-mark">*</span>}
+      </label>
+      <textarea
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className="field-textarea"
+        placeholder={placeholder}
+        required={required}
+      />
+    </div>
+  );
 };
 
 export default Textarea;
